ca74e07ed0f740c0861165ace08d4cf82921b044
Ffiirrsstt <first.engineer15@gmail.com>
2024-08-26 21:36:27 +0700

    update

--- api-shop/api-shop/Migrations/20240826143025_user-ver-5.cs
+++ api-shop/api-shop/Migrations/20240826143025_user-ver-5.cs
@@ -10,7 +10,7 @@ namespace api_shop.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+               
         }
 
         /// <inheritdoc />
